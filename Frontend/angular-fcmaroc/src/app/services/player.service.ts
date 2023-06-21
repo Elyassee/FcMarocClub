ca0d55824e4d7556212d9392d35f2df2dc57b87c
Elyassee <elyasse.bt@gmail.com>
2023-06-21 23:49:26 +0200

    Update API endpoint paths in PlayerService

--- Frontend/angular-fcmaroc/src/app/services/player.service.ts
+++ Frontend/angular-fcmaroc/src/app/services/player.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Player } from '../models/player';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,20 @@ export class PlayerService {
 
   constructor(private http: HttpClient) { }
 
-  getPlayers(): Observable<any> {
-    return this.http.get<any>(this.API_URL);
+  public getPlayers(): Observable<Player> {
+    return this.http.get<Player>(`${this.API_URL}/players`);
+  }
+
+  addPlayers(player: Player): Observable<Player> {
+    return this.http.post<Player>(`${this.API_URL}/players`, player);
+  }
+
+
+  updatePlayers(player: Player): Observable<Player> {
+    return this.http.put<Player>(`${this.API_URL}/players`, player);
+  }
+
+  deletePlayer(playerId: string): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/players${playerId}`);
   }
 }
